3f0f68362b543fb345716d8cb5a373b3112b2ca1
vovanblya <vovanblya@mail.ru>
2017-03-01 10:50:58 +0300

    1.0.5-beta (01.03.2017) ============== - Add "modAcelleObjectProcess" event - Add "processEventObject" - Add "process user" script

--- core/components/modacelle/custom/process.user.php
+++ core/components/modacelle/custom/process.user.php
@@ -1,21 +1,27 @@
 <?php
 
+echo "<pre>";
 if (!$modacelle = $modx->getService('modacelle')) {
     return;
 }
 $modacelle->initialize($modx->context->key);
 
-
 $listUid = '';
-
+$userGroup = array();
 
 $config = &$_SESSION['Console'];
 $exclude = $modx->getOption('exclude', $config, array());
 
 $q = $modx->newQuery('modUser', array('active' => 1));
+$q->innerJoin('modUserGroupMember', 'UserGroupMembers');
+
 if (!empty($exclude)) {
     $q->where(array('id:NOT IN' => $exclude));
 }
+if (!empty($userGroup)) {
+    $q->where(array('UserGroupMembers.user_group:IN' => $userGroup));
+}
+
 if ($user = $modx->getObject('modUser', $q)) {
     $exclude[] = $user->get('id');
     $config['exclude'] = $exclude;
@@ -25,7 +31,8 @@ if ($user = $modx->getObject('modUser', $q)) {
         'list_uid' => $listUid,
     ));
 
-    // print_r($data);
+    //print_r($data);
+
     $modacelle->createAcelleSubscriber($data);
     $config['completed'] = false;
 
@@ -33,6 +40,5 @@ if ($user = $modx->getObject('modUser', $q)) {
 
     $config['completed'] = true;
     $config['exclude'] = null;
-
     print_r('completed');
 }
